13de0256e3555a13fe0da41515a4c03053b046cb
Now314 <manuels0704@proton.me>
2026-06-30 10:58:16 -0500

    Prueba env y conexion DB

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,18 @@
 """Código Principal del API para BiblioOrg y su Base de Datos PostgreSQL."""
 
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from database.session import engine
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"name": "BiblioOrg API", "status": "ok"}
+    return {"message": "BiblioOrg API funcionando"}
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as conn:
+        version = conn.execute(text("SELECT version()"))
+        return {"database": version.scalar()}
