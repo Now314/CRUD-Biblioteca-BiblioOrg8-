6a8245520f5be61a3eb72933542c75a83b5c6760
Now314 <manuels0704@proton.me>
2026-06-30 11:03:02 -0500

    database --> app.database

--- api/app/database/session.py
+++ api/app/database/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database.config import DATABASE_URL
+from app.database.config import DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
