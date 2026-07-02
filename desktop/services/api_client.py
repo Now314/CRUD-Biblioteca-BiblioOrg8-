f8a879f5f7dbadcf83f4130b3ffd7bf0c1e637f1
Now314 <manuels0704@proton.me>
2026-07-02 07:45:15 -0500

    Bug api_client solucionado

--- desktop/services/api_client.py
+++ desktop/services/api_client.py
@@ -4,8 +4,12 @@ API_URL = "https://biblioorg.onrender.com"
 
 
 def get(endpoint: str):
+    url = f"{API_URL}{endpoint}"
+    print(f"Solicitando: {url}")
+
     try:
-        response = requests.get(f"{API_URL}{endpoint}", timeout=10)
+        response = requests.get(url, timeout=30)
+        print(f"Status: {response.status_code}")
         response.raise_for_status()
         return response.json()
 
